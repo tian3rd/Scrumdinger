189ca1e632643b745106e5e093f1b9420e03b025
Tian Wu <tianwu@outlook.com>
2023-12-06 21:43:38 +1100

    Set up navigation

---
+++ Scrumdinger/Views/ScrumView.swift
@@ -0,0 +1,41 @@
+//
+//  ScrumView.swift
+//  Scrumdinger
+//
+//  Created by Tian Wu on 5/12/2023.
+//
+
+import SwiftUI
+
+struct ScrumView: View {
+    let scrums: [DailyScrum]
+    
+    var body: some View {
+        NavigationStack {
+            List(scrums) { scrum in
+                NavigationLink(destination: Text(scrum.title)) {
+                    CardView(scrum: scrum)
+                }
+                .listRowBackground(scrum.theme.mainColor)
+            }
+            .navigationTitle("Daily Scrums")
+            .toolbar {
+                Button(action: {}) {
+                    Image(systemName: "plus")
+                }
+                .accessibilityLabel("New Scrum")
+            }
+        }
+    }
+}
+
+//#Preview {
+//    ScrumView()
+//}
+
+struct ScrumView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumView(scrums: DailyScrum.sampleData)
+    }
+    
+}
